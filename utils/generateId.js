af370c8be18eb045156f80cf6b6ece0d761925f2
nivek-yang <nivekyang12014@gmail.com>
2024-03-07 14:23:52 +0800

    [Feat] Add generateGroupId and generateItemId functions to utils module

---
+++ utils/generateId.js
@@ -0,0 +1,11 @@
+const { v4: uuidv4 } = require("uuid");
+
+function generateGroupId() {
+  return uuidv4();
+}
+
+function generateItemId() {
+  return uuidv4();
+}
+
+module.exports = { generateGroupId, generateItemId };
