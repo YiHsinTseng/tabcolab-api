1fb16d0d39ff5616e6bebf86c709d934231cb6c8
nivek-yang <nivekyang12014@gmail.com>
2024-03-20 15:49:55 +0800

    [Feat] Build swagger for mock-server

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,18 +1,33 @@
-process.env.DB_TYPE = 'json-server';
+require('dotenv').config();
+
+const { API_VERSION } = process.env;
+
 const jsonServer = require('json-server');
 const express = require('express');
 const apiErrorHandler = require('../middlewares/errorHandler');
 
 const server = express();
-// const router = jsonServer.router('db.json');
+
 const middlewares = jsonServer.defaults();
 
 const indexRoutes = require('../routes/index');
 
+// Redirect the root directory to the API documentation and hide api version
+server.get('/', (req, res) => {
+  res.redirect('/api-doc');
+});
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-server.use('/', indexRoutes);
+// switch swagger mode
+const swagger = require('../swaggers/config/swaggerSetup');
+
+swagger.setupSwagger(server);
+
+// api with api version
+server.use(`/api/${API_VERSION}`, indexRoutes);
+
 apiErrorHandler(server);
 
 server.listen(4000, () => {
