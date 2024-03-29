4316e8b7517dfe4a0469fb9bd8bc3ea7a8111c21
nivek-yang <nivekyang12014@gmail.com>
2024-03-29 10:37:59 +0800

    [Refactor] Move JWT authenticate to the middleware folder.

--- mock-server/server.js
+++ mock-server/server.js
@@ -6,9 +6,8 @@ const jsonServer = require('json-server');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const passport = require('../configs/passport');
+const authenticateJwt = require('../middlewares/authenticate');
 const apiErrorHandler = require('../middlewares/errorHandler');
-// switch swagger mode
 const swagger = require('../swaggers/config/swaggerSetup');
 
 const server = express();
@@ -17,34 +16,26 @@ const middlewares = jsonServer.defaults();
 
 const userRoutes = require('../routes').user;
 const groupRoutes = require('../routes').group;
-// Redirect the root directory to the API documentation and hide api version
+
+// Redirect the root directory to the API documentation
 server.get('/', (req, res) => {
   res.redirect('/api-doc');
 });
 
 swagger.setupSwagger(server);
+
 server.use(middlewares);
-// server.use(jsonServer.bodyParser);
 // 解析 JSON 請求主體
 server.use(bodyParser.json());
 // 解析 URL 編碼的請求主體
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use(express.static('public'));
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
 server.use(cors());
 
 // api with api version
 server.use(`/api/${API_VERSION}`, userRoutes);
 
-server.use((req, res, next) => {
-  console.log(req.headers);
-  next();
-});
-// JWT 驗證中間件
-const authenticate = passport.authenticate('jwt', { session: false });
 // 在所有其他路由之前添加 JWT 驗證中間件
-server.use(authenticate);
+server.use(authenticateJwt);
 server.use(`/api/${API_VERSION}`, groupRoutes);
 
 apiErrorHandler(server);
--- routes/modules/userRoutes.js
+++ routes/modules/userRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { validateRegisterandLoginDataTypes, validateUserInfoUpdateDataTypes } = require('../../validations/validation-user');
 const controller = require('../../controllers/userController');
-
+const authenticateJwt = require('../../middlewares/authenticate');
 // TODO:
-router.get('/user', controller.getAllUsers);
-router.get('/user/:user_id', controller.getUserInfo);
+router.get('/user', authenticateJwt, controller.getAllUsers);
+router.get('/user/:user_id', authenticateJwt, controller.getUserInfo);
 router.post('/user/register', validateRegisterandLoginDataTypes, controller.register);
 router.post('/user/login', validateRegisterandLoginDataTypes, controller.login);
-router.patch('/user/:user_id', validateUserInfoUpdateDataTypes, controller.updateUserInfo);
-router.delete('/user/:user_id', controller.deleteUser);
+router.patch('/user/:user_id', authenticateJwt, validateUserInfoUpdateDataTypes, controller.updateUserInfo);
+router.delete('/user/:user_id', authenticateJwt, controller.deleteUser);
 
 module.exports = router;
