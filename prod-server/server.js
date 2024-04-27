870b37d6a746f6b79dc1104ac3a9c3175df47406
nivek-yang <nivekyang12014@gmail.com>
2024-04-27 23:13:05 +0800

    [Fix] Update CORS origin in server.js

--- prod-server/server.js
+++ prod-server/server.js
@@ -56,7 +56,7 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://13.115.132.26', 'https://13.115.132.26', 'http://www.tabcolab.live', 'https://www.tabcolab.live', 'http://tabcolab.live', 'https://tabcolab.live', 'http://d1d8qny6cjcfu0.cloudfront.net', 'https://d1d8qny6cjcfu0.cloudfront.net', 'http://localhost:4000', 'chrome-extension://aiiffbafnfldcklcghpokbogbiopkagp'],
+  origin: ['http://13.115.132.26', 'https://13.115.132.26', 'http://www.tabcolab.live', 'https://www.tabcolab.live', 'http://tabcolab.live', 'https://tabcolab.live', 'http://d1d8qny6cjcfu0.cloudfront.net', 'https://d1d8qny6cjcfu0.cloudfront.net', 'http://localhost:4000', 'chrome-extension://aiiffbafnfldcklcghpokbogbiopkagp', 'chrome-extension://gfledkccocicmdgnjeafbnffcimdfonb'],
   optionsSuccessStatus: 200,
   credentials: true, // 允許跨來源的 cookie
 };
