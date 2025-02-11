dfee295383d38b07cf052eee91915375c9cf2f6a
easontseng <yhsints@gmail.com>
2025-02-11 23:03:53 +0800

    fix(tabs, sharing): Allow tab creation without browserTab_favIconURL and update sharing HTML URL

---
+++ src/routes/share.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { authenticateJwt, authenticateAdmin } = require('../middlewares/authenticate');
+const controller = require('../controllers/share');
+
+// router.get('/sharing/:id',controller.viewHtml)
+router.delete('/sharing', controller.deleteHtml);
+
+// JWT authentication middleware
+router.use(authenticateJwt);
+router.post('/sharing', controller.shareHtml);
+
+module.exports = router;
